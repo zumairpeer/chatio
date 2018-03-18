307d163acf63d0efddf5dc0388db841e4000a8e7
Peer Zumair <pzumair@sonicwall.com>
2018-03-18 22:30:18 +0530

    Added username screen, multi-user support

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ var app = express();
 var server = http.createServer(app);
 var io = socket.listen(server);
 
+var usernames = [];
+
 app.use(express.static("."));
 server.listen(3333);
 console.log('server running at port 3333');
@@ -17,10 +19,35 @@ app.get('/', function(request, response){
 io.sockets.on('connection', function(socket){
 	console.log('Socket connection established...');
 
+	socket.on('new_user', function(data, callback){
+		if(usernames.indexOf(data) != -1){
+			callback(false);
+		}
+		else {
+			callback(true);
+			socket.username = data;
+			usernames.push(socket.username);
+			updateUsernames();
+		}
+	});
+
+	function updateUsernames(){
+		io.sockets.emit('usernames', usernames);
+	}
+
+
 	socket.on('message_sent', function(message){
 		console.log('message sent' + message);
-		io.sockets.emit('new_message', {msg: message});
+		io.sockets.emit('new_message', {msg: message, user: socket.username});
 	});
+
+	socket.on('disconnect', function(data){
+		if(!socket.username){
+			return;
+		}
+		usernames.splice(usernames.indexOf(socket.username), 1);
+		updateUsernames();
+	})
 });
 
 
